afd4b51a6a5cc36e642fc1bd8dcd0ae36dab976c
Sergio <sergioricjr7@gmail.com>
2023-02-16 22:50:43 -0300

    função filtrar terminada

---
+++ js/filtrar.js
@@ -0,0 +1,71 @@
+// change é o evento de mudança de elemento, exeplo select
+const select = document.querySelector("#filter-select")
+    
+select.addEventListener("change", function(){
+        const todo = document.querySelectorAll(".todo")
+        let optionSelected = select.options[select.selectedIndex]
+
+        let value = optionSelected.value
+
+        if (value == "all"){
+            for (let i of todo){
+                i.classList.remove("hide")
+            }
+        } else if (value == "done"){
+            for (let i of todo){
+                if (!i.classList.contains("done")){
+                    i.classList.add("hide")
+                } else {
+                    i.classList.remove("hide")
+                }
+            }
+        } else if (value == "todo"){
+            for (let i of todo){
+                if (i.classList.contains("done")){
+                    i.classList.add("hide")
+                } else {
+                    i.classList.remove("hide")
+                }
+            }
+        }
+    })
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // switch (value) {
+        //     case "all":
+        //         console.log(value)
+        //         for (let i of todo){
+        //             i.classList.remove("hide")
+        //         }
+        //         break
+        //     case "done": 
+        //         for (let i of todo){
+        //             console.log(i)
+        //             if (!i.classList.contains("done")){
+        //                 i.classList.add("hide")
+        //             } else {
+        //                 i.classList.add("hide")
+        //             }
+        //         break
+        //         }   
+        //     case "todo":
+        //         console.log(value)
+        //         for (let i of todo){
+        //             if (i.classList.contains("done")){
+        //                 i.classList.add("hide")
+        //             } else {
+        //                 i.classList.remove("hide")
+        //             }
+        //         }
+
+        // }
+        
+    
